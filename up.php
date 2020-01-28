8be06cd57cf83caf9febebbd2a4b4e4e6f3b5d3f
OntoTube <35110747+OntoTube@users.noreply.github.com>
2020-01-28 16:33:37 +0300

    Fix href

--- up.php
+++ up.php
@@ -1,5 +1,5 @@
 <?php
-$calc = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham.py");
+$calc = file_get_contents("https://raw.githubusercontent.com/OntoTube/TotalCalc/master/calc.py");
 $file = fopen("calc.py", "w");
 fwrite($file, $calc);
 fclose($file); 
