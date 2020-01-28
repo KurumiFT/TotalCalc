4f245843f4494e97e9ab4a798847fc46bac9529a
OntoTube <35110747+OntoTube@users.noreply.github.com>
2020-01-28 16:21:11 +0300

    NEW

--- calc.py
+++ calc.py
@@ -1,21 +1,31 @@
 weight = int(input("Введите вес очных матчей: "))
+lastweight = int(input("Вес последних матчей: "))
 
 xc = input("Тоталы очных матчей(через запятую): ")
 xa = input("Тоталы 1-ой команды(через запятую): ")
 xb = input("Тоталы 2-ой команды(через запятую): ")
 
-xc = str.split(xc,",")
-xa = str.split(xa,",")
-xb = str.split(xb,",")
+xc = str.split(xc, ",")
+xa = str.split(xa, ",")
+xb = str.split(xb, ",")
 
-print(xc)
-print(xa)
-print(xb)
+print("---------")
+print("Проверьте правильность введёных данных: ")
+print("Тоталы очных: "+str(xc))
+print("Тоталы 1-ой команды: "+str(xa))
+print("Тоталы 2-ой команды: "+str(xb))
+
 
 def summ(arr):
+    kol = 0
+    tempweight = lastweight
     summ = 0
     for i in arr:
-        summ = summ + float(i)
-    return  summ
+        summ = summ + (float(i) * tempweight)
+        kol = kol + tempweight
+        tempweight = max(1, tempweight - 1)
+    return summ / kol
 
-print(((weight*(summ(xc)/len(xc)))+(summ(xa)/len(xa))+(summ(xb)/len(xb)))/(2+weight))
+
+print("\n")
+print("Средний тотал(ставьте меньше): "+str((weight*summ(xc)+summ(xa)+summ(xb))/(2+weight)))
