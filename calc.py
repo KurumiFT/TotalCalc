0befea18f89bd4b6637feba71068e9cf78d627fb
OntoTube <35110747+OntoTube@users.noreply.github.com>
2020-01-30 16:19:59 +0300

    Global update

--- calc.py
+++ calc.py
@@ -1,13 +1,10 @@
+wins = input("Введите победы первой команды и второй в очных: ").split(",")
 weight = int(input("Введите вес очных матчей: "))
 lastweight = int(input("Вес последних матчей: "))
 
-xc = input("Тоталы очных матчей(через запятую): ")
-xa = input("Тоталы 1-ой команды(через запятую): ")
-xb = input("Тоталы 2-ой команды(через запятую): ")
-
-xc = str.split(xc, ",")
-xa = str.split(xa, ",")
-xb = str.split(xb, ",")
+xc = input("Тоталы очных матчей(через запятую): ").split(",")
+xa = input("Тоталы 1-ой команды(через запятую): ").split(",")
+xb = input("Тоталы 2-ой команды(через запятую): ").split(",")
 
 print("---------")
 print("Проверьте правильность введёных данных: ")
@@ -15,6 +12,19 @@ print("Тоталы очных: "+str(xc))
 print("Тоталы 1-ой команды: "+str(xa))
 print("Тоталы 2-ой команды: "+str(xb))
 
+def max_arr(arr):
+    max = 0
+    for i in xc:
+        if i > max:
+            max = i
+    return max
+
+def min_arr(arr):
+    min = 1000
+    for i in xc:
+        if i < min:
+            min = i
+    return min
 
 def summ(arr):
     kol = 0
@@ -26,6 +36,13 @@ def summ(arr):
         tempweight = max(1, tempweight - 1)
     return summ / kol
 
+normal = (weight*summ(xc)+summ(xa)+summ(xb))/(2+weight)
 
 print("\n")
-print("Средний тотал(советую занижать): "+str((weight*summ(xc)+summ(xa)+summ(xb))/(2+weight)))
+print("Средний тотал(не ориентируйтесь на него): "+str(normal))
+print("---------------------------------------")
+
+if (max(int(wins[0]),int(wins[1])) - min(int(wins[0]),int(wins[1])) == 0) or (max(int(wins[0]),int(wins[1])) - min(int(wins[0]),int(wins[1])) == 1):
+    print("Рекомендуем ставить на Тотал {Total}М".format(Total=(normal+max_arr(xc))/2))
+else:
+    print("Рекомендуем ставить на Тотал {Total}Б".format(Total=((normal+min_arr(xc))/2)))
